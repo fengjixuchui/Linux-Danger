60a5e1c8c4743ab1133285481ccbd57ea85c68f7
UEFI-code <zhuoshizhang@hotmail.com>
2025-09-22 10:14:59 +0900

    Add egg

--- kernel/sched/easy.c
+++ kernel/sched/easy.c
@@ -21,10 +21,6 @@ struct kthread {
 
 #define EASY_MAX_TASKS 8192
 
-// static struct task_struct **tasks = NULL;
-// static int task_count = 0;
-// static int current_index = 0;
-
 typedef struct {
     struct task_struct **tasks;
     int task_count;
@@ -33,12 +29,26 @@ typedef struct {
 
 easy_sched_struct_def easy_cpu_contexts[128] = {0};
 
+__inline void egg(void)
+{
+    static uint8_t shown = 0;
+    if (shown) return;
+    const char *unix_day[] = {"Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday"};
+    pr_alert("easy_sched: From Beijing to Hiroshima, Professors kept saying the same thing:\n");
+    pr_alert("easy_sched: 'Your math is too poor!'\n");
+    pr_alert("easy_sched: But every time they said it, I had already deleted their math-heavy bullshit,\n");
+    pr_alert("easy_sched: and the system still ran — faster, cleaner, simpler.\n");
+    pr_alert("!!! Happy %s !!!\n", unix_day[(__ktime_get_real_seconds()/86400)%7]);
+    shown = 1;
+}
+
 void easy_sched_init(easy_sched_struct_def *easy_context)
 {
     easy_context->tasks = kmalloc_array(EASY_MAX_TASKS, sizeof(void *), GFP_KERNEL);
     if (!easy_context->tasks) BUG();
     memset(easy_context->tasks, 0, EASY_MAX_TASKS * sizeof(void *));
     pr_alert("!!! easy_sched_init succ !!!\n");
+    egg();
 }
 
 static void enqueue_task_easy(struct rq *rq, struct task_struct *p, int flags)
@@ -105,7 +115,7 @@ static void yield_task_easy(struct rq *rq)
     easy_context->index++;
 }
 
-static void put_prev_task_easy(struct rq *rq, struct task_struct *p) {}
+static void put_prev_task_easy(struct rq *rq, struct task_struct *p) { }
 static void set_next_task_easy(struct rq *rq, struct task_struct *p, bool first) { }
 static void check_preempt_curr_easy(struct rq *rq, struct task_struct *p, int flags) { }
 static void task_tick_easy(struct rq *rq, struct task_struct *p, int queued) { }
@@ -154,33 +164,25 @@ DEFINE_SCHED_CLASS(easy) =
     .dequeue_task       = dequeue_task_easy,
     .yield_task         = yield_task_easy,
     .yield_to_task      = yield_to_task_easy,
-
-    .check_preempt_curr = check_preempt_curr_easy,
-
     .pick_next_task     = pick_next_task_easy,
     .put_prev_task      = put_prev_task_easy,
     .set_next_task      = set_next_task_easy,
-
+    .check_preempt_curr = check_preempt_curr_easy,
 #ifdef CONFIG_SMP
     .balance            = balance_easy,
     .pick_task          = pick_next_task_easy,
     .select_task_rq     = select_task_rq_easy,
     .migrate_task_rq    = migrate_task_rq_easy,
-
     .rq_online          = rq_online_easy,
     .rq_offline         = rq_offline_easy,
-
     .task_dead          = task_dead_easy,
     .set_cpus_allowed	= set_cpus_allowed_common,
 #endif
-
     .task_tick           = task_tick_easy,
     .task_fork           = task_fork_easy,
-
     .prio_changed        = prio_changed_easy,
     .switched_from       = switched_from_easy,
     .switched_to         = switched_to_easy,
-
     .get_rr_interval     = get_rr_interval_easy,
     .update_curr         = update_curr_easy,
 #ifdef CONFIG_SCHED_CORE
